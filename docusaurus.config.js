3a54f73c50596fc9bb2c60b5abf6ef16c20e4c9e
JastinXyz <jastinlinggar@gmail.com>
2022-12-07 14:14:51 +0700

    v1.0.3 update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
           lastVersion: "current",
           versions: {
             current: {
-              label: "1.0.2",
+              label: "1.0.3",
             },
           },
           sidebarPath: require.resolve("./sidebars.js"),
