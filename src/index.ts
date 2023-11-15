9e9c41b24b85ebb601936facdf3096b852eeb1c9
JastinXyz <jastinlinggar@gmail.com>
2023-11-15 15:09:32 +0700

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-// import { Client } from "./Classes/Client";
-// import { CommandHandler } from "./Classes/CommandHandler";
-// import { ButtonBuilder } from "./Classes/Builder/Button";
-// import { SectionsBuilder } from "./Classes/Builder/Sections";
-// import { Cooldown } from "./Classes/Cooldown";
-// import { VCardBuilder } from "./Classes/Builder/VCard";
-// import { TemplateButtonsBuilder } from "./Classes/Builder/TemplateButtons";
-
-// export = {
-//     Client,
-//     CommandHandler,
-//     Cooldown,
-//     ButtonBuilder,
-//     SectionsBuilder,
-//     VCardBuilder,
-//     TemplateButtonsBuilder
-// };
-
 export * from "./Classes/Client";
 export * from "./Classes/CommandHandler";
 export * from "./Classes/Builder/Button";
