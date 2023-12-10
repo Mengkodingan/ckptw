5f589d9987a6beaabbbe17fe7e8a665f4c997349
JastinXyz <jastinlinggar@gmail.com>
2023-12-10 12:28:45 +0700

    feat: ctx.deleteMessage

--- src/Classes/Ctx.ts
+++ src/Classes/Ctx.ts
@@ -114,4 +114,8 @@ export class Ctx implements CtxInterface {
     simulateTyping() {
         this._client.sendPresenceUpdate('composing', this.id)
     }
+
+    async deleteMessage(key: any) {
+        return this._client.sendMessage(this.id, { delete: key });
+    }
 }
