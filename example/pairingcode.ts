fb1938a854b01402b624c5cbab8b028561727680
JastinXyz <jastinlinggar@gmail.com>
2024-05-06 09:34:52 +0700

    forgot to remove this

--- example/pairingcode.ts
+++ example/pairingcode.ts
@@ -5,7 +5,7 @@ const bot = new Client({
     printQRInTerminal: false,
     readIncommingMsg: true,
     usePairingCode: true,
-    phoneNumber: '6285931594034'
+    phoneNumber: '' // phone number, e.g 62xxxxx
 });
 
 bot.ev.once(Events.ClientReady, (m) => {
